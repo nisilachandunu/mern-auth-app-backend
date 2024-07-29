3446c1a40385defd7b8e4bd4ee6c1e4e4debd040
Nisila Bambarenda <nisila.bambarenda@innovative-e.com>
2024-07-29 23:52:26 +0530

    connected db

--- API/index.js
+++ API/index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongo DB");
+  })
+  .catch((err) => console.log(err));
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
